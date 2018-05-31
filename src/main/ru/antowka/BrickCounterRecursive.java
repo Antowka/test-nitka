e7a7dd28b260e8b22f3a80515b48500dbd8d0e47
Anton Nikanorov <anikanor@mera.ru>
2018-06-01 02:17:04 +0300

    update for recursive version

--- src/main/ru/antowka/BrickCounterRecursive.java
+++ src/main/ru/antowka/BrickCounterRecursive.java
@@ -4,32 +4,28 @@ public class BrickCounterRecursive implements BrickCounter {
 
     @Override
     public double weight(int row, int pos) {
-
         if (row == 0) {
             return 0;
         }
-
-        return getWeightWithPrevLevel(row, pos, 0, 0.5d);
+        return getWeight(row, pos);
     }
 
 
-
-    private double getWeightWithPrevLevel(int row, int pos, double result, double step) {
-
-        if (row == 0) return result; //exit
+    private double getWeight(int row, int pos) {
 
 
+        if (row == 0) {
+            return 0;
 
-        if (isFirstOrLastBrickInRow(row, pos)) {
-            if (pos != 0) pos--; //for only last block in row
-            result += step;
-            return getWeightWithPrevLevel(--row, pos, result, step / 2);
+        } else if (pos == 0 || pos == row) {
+            double tempVal = 0d;
+            for (int i = 1; i <= row; i++) {
+                tempVal += Math.pow(0.5d, i);
+            }
+            return tempVal;
+        } else {
+
+            return (getWeight(row - 1, pos - 1) + getWeight(row - 1, pos) + 2) / 2;
         }
-
-        row--;
-        double leftParentWeight = getWeightWithPrevLevel(row, pos - 1, result, step);
-        double rightParentWeight = getWeightWithPrevLevel(row, pos, result, step);
-
-        return (leftParentWeight + rightParentWeight + 2) / 2;
     }
 }
